77de8ea289748d8b4d46a722a96edc0648301726
Miguel Inza <inza.miguel@gmail.com>
2022-12-12 23:32:41 +0100

    final commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 //const supabaseKey = "poiuytrewq0987654321|@#~€¬";
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
